ff7a4a3d8dd80c15f5bdc6c6dde6f0f1430fd2c1
ashwinanand7 <ashwin.rjn793@gmail.com>
2026-03-13 16:54:37 +0000

    Deploying to gh-pages from @ ashwinanand7/ashwinanand7.github.io@dd43153c67ba8a30cd28c109d1fd2f079fa16457 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "See a record of all the cool stuff I&#39;ve done before now!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
