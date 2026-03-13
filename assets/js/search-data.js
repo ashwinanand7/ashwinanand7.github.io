d63a735c75f03217e0265b7bd4a23ff89e0f4416
ashwinanand7 <ashwin.rjn793@gmail.com>
2026-03-13 16:37:46 +0000

    Deploying to gh-pages from @ ashwinanand7/ashwinanand7.github.io@8c721729c84a309c36e43425cb30f293b01d9ba3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,9 +423,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-vector-valued-optimal-transport",
+          title: 'Vector-Valued Optimal Transport',
+          description: "Using OT methods to compare classifiers",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
