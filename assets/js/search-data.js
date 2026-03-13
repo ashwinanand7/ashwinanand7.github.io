73d25e77e64e5d3e042ca358b930623df5858c70
ashwinanand7 <ashwin.rjn793@gmail.com>
2026-03-13 16:52:12 +0000

    Deploying to gh-pages from @ ashwinanand7/ashwinanand7.github.io@7ba9890eb3d5dcd939a36b653ccdf616e0a314ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Cool math I am doing!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
